7b7b992b31ec7a79ae635fd76aecd1dc30230395
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-08-11 16:31:09 -0600

    Added light useState to the Parent (App) then passed down as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import './App.css'
+import { useState } from 'react'
 import { AngryButton } from './components/AngryButton'
 import { CounterButton } from './components/CounterButton'
 import { LightSwitchButton } from './components/LightSwitchButton'
 import { TextRepeaterButton } from './components/TextRepeaterButton'
 
 export const App = () => {
+  const [light, setLight] = useState(0);
+  const dark = (light === 0) ? 'dark' : '';
+  // (light === 0) && 'dark' || (light === 1) && ''
+
   return (
-    <div className="App">
+    <div className={`App ${dark}`}>
       <h1>Fancy Buttons</h1>
-      
+
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} setLight={setLight}/>
         <TextRepeaterButton />
       </section>
     </div>
--- src/components/LightSwitchButton.jsx
+++ src/components/LightSwitchButton.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react"
-
 export const LightSwitchButton = (props) => {
-  const [light, setLight] = useState(0);
+  // const [light, setLight] = useState(0);
+  const {light, setLight} = props;
 
   const handleClick = () => light === 0 && setLight(1) || light === 1 && setLight(0)
                             // setLight(light === "1" ? "0" : "1");
   
-
   return (
     <button onClick={handleClick} className="LightSwitchButton">
     {light === 1 && <span><i>💡</i> I&#39;m on!</span>}
