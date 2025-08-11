1118c942ff895a8f26332a8c9a322ff6096225c2
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-08-11 17:21:36 -0600

    Used useReducer in place of State for one of the React states

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
 import { AngryButton } from './components/AngryButton'
 import { CounterButton } from './components/CounterButton'
 import { LightSwitchButton } from './components/LightSwitchButton'
 import { TextRepeaterButton } from './components/TextRepeaterButton'
 
 export const App = () => {
-  const [light, setLight] = useState(0);
-
-  const [angryApp, setAngryApp] = useState(0)
-  const increaseAnger = (amount) => {
+  const [angryApp, dispatch] = useReducer((angryApp, amount) => {
     if (angryApp < 1) {
-      setAngryApp(angryApp + amount);
+      return angryApp + amount;
     } else {
-      setAngryApp(0);
+      return 0;
     }
-  }
+  }, 0);
 
+  // const increaseAnger = (amount) => {
+  //   if (angryApp < 1) {
+  //     setAngryApp(angryApp + amount);
+  //   } else {
+  //     setAngryApp(0);
+  //   }
+  // }
+
+  const [light, setLight] = useState(0);
   const switchLight = () => {
     light === 0 && setLight(1) || light === 1 && setLight(0)
     // setLight(light === "1" ? "0" : "1");
-    increaseAnger(0.1)
-  }
-                         
+    dispatch(0.1)
+  }                   
   const dark = (light === 0) ? 'dark' : '';
             // (light === 0) && 'dark' || (light === 1) && ''
 
@@ -30,10 +35,10 @@ export const App = () => {
     <div className={`App ${dark}`}>
       <h1>{angryApp < 1 ? "Fancy Buttons!" : "YOU'RE CLICKING TOO MANY BUTTONS!"}</h1>
       <section>
-        <AngryButton increaseAnger={increaseAnger}/>
-        <CounterButton increaseAnger={increaseAnger}/>
+        <AngryButton increaseAnger={dispatch}/>
+        <CounterButton increaseAnger={dispatch}/>
         <LightSwitchButton light={light} switchLight={switchLight} />
-        <TextRepeaterButton increaseAnger={increaseAnger}/>
+        <TextRepeaterButton increaseAnger={dispatch}/>
       </section>
     </div>
   )
